3e31fb550cad15756f4672477006297f1b9f2109
Nats <dfsd>
2019-09-26 20:00:50 -0600

    mas cambios

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
 import funciones
 import objeto
 
+while True:
+    opcion=int(input("Que desea hacer?\n 1.Sumar\n 2.Restar \n 3.Multiplicar \n 4.Dividir \n5.Salir \n>"))
+    if opcion==1:
+        valor1=int(input("Valor para sumar: "))
+        valor2=int(input("Valor para ser sumado: "))
+        print(funciones.suma(valor1,valor2))
+    elif opcion==2:
+        valor1=int(input("Valor para restar: "))
+        valor2=int(input("Valor para ser restado: "))
+        print(funciones.resta(valor1,valor2))
+    elif opcion==3:
+        valor1=int(input("Valor para multiplicar: "))
+        valor2=int(input("Valor para ser multiplicado: "))
+        print(funciones.multiplicar(valor1,valor2))
+    elif opcion==4:
+        valor1=int(input("Valor para dividir: "))
+        valor2=int(input("Valor para ser dividido: "))
+        print(funciones.dividir(valor1,valor2))
+    elif opcion==5:
+        break
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask
+# from flask import Flask
 
-app= Flask(__name__)
-# conn=sqlite(./db)
-# clases
-@app.route('/')
-def hello():
-    return "Hola mundo"
+# app= Flask(__name__)
+# # conn=sqlite(./db)
+# # clases
+# @app.route('/')
+# def hello():
+#     return "Hola mundo"
 
-if __name__=='__main__':
-    app.run()
+# if __name__=='__main__':
+#     app.run()
+
+class person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+p1=person("Jhon",23)
+print(p1.name)
+print(p1.age)
+
+del p1
